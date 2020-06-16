e8e0a6264c71be6116dd2035ac103efd522cd168
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-06-16 17:24:51 -0300

    feat: add new entities

--- Store.Tests/UnitTest1.cs
+++ Store.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.StoreContext.Entities;
 
 namespace Store.Tests
 {
@@ -8,6 +9,13 @@ namespace Store.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var c = new Customer(
+                "André",
+                "Baltieri",
+                "12345678911",
+                "hello@balta.io",
+                "19999999999",
+                "Rua dos Developers, 1024");
         }
     }
 }
