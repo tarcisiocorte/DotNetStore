8513d4e2ffa9ce2d26ddc5e3dde0c2affdc4f5d7
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-06-22 23:51:36 -0300

    chore: update tests

--- Store.Tests/UnitTest1.cs
+++ Store.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Store.Tests
 {
@@ -9,13 +10,35 @@ namespace Store.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Customer(
-                "Andr�",
-                "Baltieri",
-                "12345678911",
-                "hello@balta.io",
-                "19999999999",
-                "Rua dos Developers, 1024");
+            var name = new Name("André", "Da Silva");
+            var document = new Document("12345678911");
+            var email = new Email("mano@testemail.com");
+            var c = new Customer(name, document, email, "1999207414");
+            var mouse = new Product("Mouse", "Rato", "image.png", 59.90M, 10);
+            var teclado = new Product("Teclado", "Teclado", "image.png", 159.90M, 10);
+            var impressora = new Product("Impressora", "Impressora", "image.png", 359.90M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "image.png", 559.90M, 10);
+
+            var order = new Order(c);
+            //order.AddItem(new OrderItem(mouse, 5));
+            //order.AddItem(new OrderItem(teclado, 5));
+            //order.AddItem(new OrderItem(cadeira, 5));
+            //order.AddItem(new OrderItem(impressora, 5));
+
+            // Realizei o pedido
+            order.Place();
+
+            // Verificar se o pedido é valido
+            //var valid = order.IsValid;
+
+            // Simular o pagamento
+            order.Pay();
+
+            // Simular o envio
+            order.Ship();
+
+            // Simular o cancelamento
+            order.Cancel();
         }
     }
 }
